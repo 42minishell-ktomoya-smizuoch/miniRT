2d188639493530ad04762c7950ca0eefebbda0a8
smizuoch <shotaro.mizuochi@proton.me>
2024-02-23 15:47:46 +0900

    first commit

---
+++ includes/minirt.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smizuoch <smizuoch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/23 15:35:43 by smizuoch          #+#    #+#             */
+/*   Updated: 2024/02/23 15:40:09 by smizuoch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <math.h>
+
+# include "mlx.h"
+# include "libft.h"
+
+#endif
