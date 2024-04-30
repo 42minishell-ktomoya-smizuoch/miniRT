9e6d4726996489257c31355787c9b4330316a9c1
smizuoch <shotaro.mizuochi@proton.me>
2024-04-30 15:28:45 +0900

    add atof

--- main.c
+++ main.c
@@ -12,24 +12,27 @@ int main(void)
 	y = 0;
 
 	// 使用例
-    const char *data = "123 456 test A";
+    const char *data = "123 456 test A 3.1415926";
+	double d;
     int n1, n2;
     char str[10], ch;
 
-    if (ft_sscanf(data, "%d %d %s %c", &n1, &n2, str, &ch) == 4) {
+    if (ft_sscanf(data, "%d %d %s %c %lf", &n1, &n2, str, &ch ,&d) == 5) {
         printf("Read int: %d\n", n1);
         printf("Read int: %d\n", n2);
         printf("Read string: %s\n", str);
         printf("Read char: %c\n", ch);
+		printf("Read double: %lf\n", d);
     } else {
         printf("Error in sscanf\n");
     }
 	printf("=====================================\n");
-	sscanf(data, "%d %d %s %c", &n1, &n2, str, &ch);
+	sscanf(data, "%d %d %s %c %lf", &n1, &n2, str, &ch ,&d);
 	printf("Read int: %d\n", n1);
 	printf("Read int: %d\n", n2);
 	printf("Read string: %s\n", str);
 	printf("Read char: %c\n", ch);
+	printf("Read double: %lf\n", d);
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "Hello world!");
