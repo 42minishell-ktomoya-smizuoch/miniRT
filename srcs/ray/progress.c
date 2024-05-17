d802a34c2c4f57b2ac9c8e5c11db0155b9bc4d1b
smizuoch <shotaro.mizuochi@proton.me>
2024-05-17 17:42:18 +0900

    comment add

--- srcs/ray/progress.c
+++ srcs/ray/progress.c
@@ -6,7 +6,7 @@
 /*   By: smizuoch <smizuoch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 14:55:07 by smizuoch          #+#    #+#             */
-/*   Updated: 2024/05/17 12:25:59 by smizuoch         ###   ########.fr       */
+/*   Updated: 2024/05/17 17:33:38 by smizuoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,3 +40,29 @@ void	print_progress(int current, int total)
 	else
 		printf("\n");
 }
+
+// void	print_progress(int current, int total)
+// {
+// 	int		bar_width;
+// 	float	progress;
+// 	int		pos;
+// 	int		i;
+
+// 	bar_width = 70;
+// 	progress = (float)current / total;
+// 	pos = bar_width * progress;
+// 	printf("[");
+// 	i = 0;
+// 	while (i < bar_width)
+// 	{
+// 		if (i < pos)
+// 			printf("=");
+// 		else if (i == pos)
+// 			printf(">");
+// 		else
+// 			printf(" ");
+// 		i++;
+// 	}
+// 	printf("] %d%%\r", (int)(progress * 100.0));
+// 	fflush(stdout);
+// }
