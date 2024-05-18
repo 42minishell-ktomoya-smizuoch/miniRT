02403f9fefacab2c934039a83469888895d55c15
tom <134240277+ktomoya1@users.noreply.github.com>
2024-05-18 17:44:54 +0900

    スフィアの構造体に数値を設定する実装を追加

--- playground/srcs/parser.c
+++ playground/srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: ktomoya <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 09:06:40 by ktomoya           #+#    #+#             */
-/*   Updated: 2024/05/17 17:25:07 by ktomoya          ###   ########.fr       */
+/*   Updated: 2024/05/18 17:36:55 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,20 +40,40 @@ int	main(int argc, char *argv[])
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		exit(3);
-	char	*text;
+	char		*text;
+	char		type[10] = {0};
+	int			i;
+	t_sphere	sphere;
 	
+	sphere.center.x = 0;
+	sphere.center.y = 0;
+	sphere.center.z = 0;
 	text = NULL;
 	while (1)
 	{
+		i = 0;
 		text = get_next_line(fd);
 		if (text == NULL) {
 			printf("get_next_line return NULL\n");
 			break ;
 		} else if (*text == '\n') {
 			printf("newline\n");
-		} else {
-			
-			printf("%s", text);
+		} else { 
+			// “sp 0.0,0.0,20.6 12.6  10,0,255”
+			while (text[i] == ' ')
+				i++;
+			printf("&text[i]:%s\n", &text[i]);
+			if (ft_strncmp(&text[i], "sp", ft_strlen("sp")) == 0) {
+				if (sscanf(text, "%s %lf,%lf,%lf %lf %d,%d,%d", type,
+					&sphere.center.x, &sphere.center.y, &sphere.center.z, &sphere.diameter, &sphere.color.r, &sphere.color.g, &sphere.color.b) != 8) {
+					printf("x:%lf, y:%lf, z:%lf, diameter:%lf\n, r:%d, g:%d, b:%d",
+					sphere.center.x, sphere.center.y, sphere.center.z, sphere.diameter, sphere.color.r, sphere.color.g, sphere.color.b);
+					printf("Error1\n");
+					exit(1);
+				}
+				printf("x:%lf, y:%lf, z:%lf, diameter:%lf, r:%d, g:%d, b:%d\n",
+					sphere.center.x, sphere.center.y, sphere.center.z, sphere.diameter, sphere.color.r, sphere.color.g, sphere.color.b);
+			}
 		}
 		free(text);
 		text = NULL;
