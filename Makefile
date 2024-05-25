211a63da20f9c261d5805d6fe8bd37caba390aec
smizuoch <shotaro.mizuochi@proton.me>
2024-05-25 17:29:16 +0900

    時間を追加

--- Makefile
+++ Makefile
@@ -32,6 +32,7 @@ SRC += srcs/ray/ray_math.c \
 	   srcs/ray/metal.c \
 	   srcs/ray/dielectric.c \
 	   srcs/ray/camera.c \
+	   srcs/ray/moving_sphere.c \
 
 SRC += srcs/mlx/my_mlx_pixel_put.c \
 
--- includes/camera.h
+++ includes/camera.h
@@ -3,7 +3,7 @@
 
 #include "minirt.h"
 
-t_camera camera_new(t_vec3 lookfrom, t_vec3 lookat, t_vec3 vup, double vfov, double aspect_ratio, double aperture, double focus_dist);
+t_camera camera_new(t_vec3 lookfrom, t_vec3 lookat, t_vec3 vup, double vfov, double aspect_ratio, double aperture, double focus_dist, double time0, double time1);
 t_ray get_ray(t_camera *cam, double s, double t);
 
 #endif
