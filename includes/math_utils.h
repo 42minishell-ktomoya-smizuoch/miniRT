b22ab9f36264258e824da6adc331c177cd6a5132
smizuoch <shotaro.mizuochi@proton.me>
2024-06-04 11:47:58 +0900

    fix norm MATH_UTILS_H

--- includes/math_utils.h
+++ includes/math_utils.h
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   math_utils.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smizuoch <smizuoch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/04 11:47:00 by smizuoch          #+#    #+#             */
+/*   Updated: 2024/06/04 11:47:29 by smizuoch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MATH_UTILS_H
-#define MATH_UTILS_H
+# define MATH_UTILS_H
 
-// 数学定数
-#define PI 3.1415926535897932385
+//Mathematical constants
+# define PI 3.1415926535897932385
 
-// 度をラジアンに変換
-double degrees_to_radians(double degrees);
+//Convert degrees to radians
+double	degrees_to_radians(double degrees);
 
 #endif
