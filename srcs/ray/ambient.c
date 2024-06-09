b8b769a4728b25c3764873e4cf5ddbc5e22cfb34
smizuoch <shotaro.mizuochi@proton.me>
2024-06-09 16:30:52 +0900

    目についたnormを直す

--- srcs/ray/ambient.c
+++ srcs/ray/ambient.c
@@ -1,8 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ambient.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smizuoch <smizuoch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/09 16:18:38 by smizuoch          #+#    #+#             */
+/*   Updated: 2024/06/09 16:18:39 by smizuoch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ambient.h"
 
-t_ambient new_ambient(t_color color, double ratio) {
-    t_ambient ambient;
-    ambient.color = color;
-    ambient.ratio = ratio;
-    return ambient;
+t_ambient	new_ambient(t_color color, double ratio)
+{
+	t_ambient	ambient;
+
+	ambient.color = color;
+	ambient.ratio = ratio;
+	return (ambient);
 }
