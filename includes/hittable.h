30d4c6f7408553e9442ac15795345a706f64fe7e
smizuoch <shotaro.mizuochi@proton.me>
2024-06-15 16:52:20 +0900

    ヘッダーのnorm直し

--- includes/hittable.h
+++ includes/hittable.h
@@ -6,7 +6,7 @@
 /*   By: smizuoch <smizuoch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 09:10:40 by smizuoch          #+#    #+#             */
-/*   Updated: 2024/06/15 16:34:25 by smizuoch         ###   ########.fr       */
+/*   Updated: 2024/06/15 16:42:12 by smizuoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ typedef struct s_hit_record
 	double			t;
 	int				front_face;
 	t_color			color;
-	double			fuzz; // Blurring degree of metal
-	double			ref_idx; // Refractive index
-	t_material_type	material; // Material Type
+	double			fuzz;
+	double			ref_idx;
+	t_material_type	material;
 }	t_hit_record;
 
 struct		s_hittable;
@@ -47,14 +47,18 @@ typedef struct s_hittable_list
 	int			capacity;
 }	t_hittable_list;
 
-void			set_face_normal(t_hit_record *rec, t_ray *r,t_vec3 outward_normal);
-void			set_face_normal_with_radius(t_hit_record *rec, t_ray *r, t_vec3 outward_normal, double radius);
+void			set_face_normal(t_hit_record *rec,
+					t_ray *r, t_vec3 outward_normal);
+void			set_face_normal_with_radius(t_hit_record *rec,
+					t_ray *r, t_vec3 outward_normal, double radius);
 t_hittable_list	*new_hittable_list(int initial_capacity);
 void			add_hittable(t_hittable_list *list, t_hittable object);
-int				hit_list(t_hittable_list *list, t_ray *ray, t_limits limit, t_hit_record *rec);
+int				hit_list(t_hittable_list *list,
+					t_ray *ray, t_limits limit, t_hit_record *rec);
 void			free_hittable_list(t_hittable_list *list);
 
-int				hit_sphere(t_hittable *self, t_ray *ray, t_limits l, t_hit_record *rec);
+int				hit_sphere(t_hittable *self,
+					t_ray *ray, t_limits l, t_hit_record *rec);
 t_hittable		new_sphere(t_vec3 center, double radius, t_color color);
 
 #endif
