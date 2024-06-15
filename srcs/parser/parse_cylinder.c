40ebc317d6b3d91794bef0b53ff070f4119d7a43
tom <134240277+ktomoya1@users.noreply.github.com>
2024-06-15 16:33:19 +0900

    Added files that was forgotten in staging.

---
+++ srcs/parser/parse_cylinder.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_cylinder.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktomoya <twbtomoya2@student.42.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/15 16:26:22 by ktomoya           #+#    #+#             */
+/*   Updated: 2024/06/15 16:28:49 by ktomoya          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+void	parse_cylinder(const char *line, t_cylinder *cylinder)
+{
+		if (sscanf(line, "%lf,%lf,%lf %lf,%lf,%lf %lf %lf %lf,%lf,%lf", &cylinder->center.x, &cylinder->center.y, &cylinder->center.z, &cylinder->axisnorm.x, &cylinder->axisnorm.y, &cylinder->axisnorm.z, &cylinder->diameter, &cylinder->height, &cylinder->color.r, &cylinder->color.g, &cylinder->color.b) != 11)
+			exit_with_error("Errorcy\n");
+}
