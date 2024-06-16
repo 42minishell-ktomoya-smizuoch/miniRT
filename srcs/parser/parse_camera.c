af20e92b33c594a02a31db72ff051b37d6aaae86
tom <134240277+ktomoya1@users.noreply.github.com>
2024-06-16 14:02:31 +0900

    Set values for lens_radius and focus_distance

--- srcs/parser/parse_camera.c
+++ srcs/parser/parse_camera.c
@@ -6,7 +6,7 @@
 /*   By: ktomoya <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 16:37:14 by ktomoya           #+#    #+#             */
-/*   Updated: 2024/06/16 07:31:52 by ktomoya          ###   ########.fr       */
+/*   Updated: 2024/06/16 13:48:40 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 void	parse_camera(const char *line, t_camera *camera)
 {
-	if (ft_sscanf(line, "%lf,%lf,%lf %lf,%lf,%lf %lf %lf,%lf,%lf",
+	if (ft_sscanf(line, "%lf,%lf,%lf %lf,%lf,%lf %lf %lf,%lf,%lf %lf %lf",
 			&camera->origin.x, &camera->origin.y, &camera->origin.z,
 			&camera->normal.x, &camera->normal.y, &camera->normal.z,
 			&camera->fov, &camera->lower_left_corner.x,
-			&camera->lower_left_corner.y, &camera->lower_left_corner.z) != 10)
+			&camera->lower_left_corner.y, &camera->lower_left_corner.z,
+			&camera->lens_radius, &camera->focus_distance) != 12)
 		exit_with_error("Error:C\n");
 	if (is_vec3_out_of_range(camera->normal, -1.0, 1.0) == true)
 		exit_with_error("Error:C:vec3:out of ragne\n");
--- srcs/print_struct.c
+++ srcs/print_struct.c
@@ -6,7 +6,7 @@
 /*   By: ktomoya <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 17:07:47 by ktomoya           #+#    #+#             */
-/*   Updated: 2024/06/16 07:45:17 by ktomoya          ###   ########.fr       */
+/*   Updated: 2024/06/16 13:46:52 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,18 +31,21 @@ void	print_struct(t_data data)
        "fov       : %lf\n"
        "lower.x   : %lf\n"
        "lower.y   : %lf\n"
-       "lower.z   : %lf\n",
+       "lower.z   : %lf\n"
+       "aperture  : %lf\n"
+       "distance  : %lf\n",
        data.camera.origin.x, data.camera.origin.y, data.camera.origin.z, 
        data.camera.normal.x, data.camera.normal.y, data.camera.normal.z, 
        data.camera.fov,
 	   data.camera.lower_left_corner.x, data.camera.lower_left_corner.y,
-	   data.camera.lower_left_corner.z);
+	   data.camera.lower_left_corner.z,
+	   data.camera.lens_radius, data.camera.focus_distance);
 
 	printf("----------------------------light----------------------------------\n"
        "origin.x  : %lf\n"
        "origin.y  : %lf\n"
        "origin.z  : %lf\n"
-       "ratio     : %lf\n"
+       "intensity : %lf\n"
        "color.r   : %lf\n"
        "color.g   : %lf\n"
        "color.b   : %lf\n",
