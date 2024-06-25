bb6b9c5b891caa9f0a8c1c88b5d0a9371519bb79
ktomoya <twbtomoya2@gmail.com>
2024-06-25 16:32:52 +0900

    fix norminette

--- srcs/parser/scan_camera.c
+++ srcs/parser/scan_camera.c
@@ -6,7 +6,7 @@
 /*   By: smizuoch <smizuoch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 10:52:27 by ktomoya           #+#    #+#             */
-/*   Updated: 2024/06/25 13:25:50 by smizuoch         ###   ########.fr       */
+/*   Updated: 2024/06/25 16:29:45 by ktomoya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,17 @@ void	scan_camera(const char *line, t_init_cam *setter)
 	if (ft_sscanf(line, "%lf,%lf,%lf %lf,%lf,%lf %lf %lf,%lf,%lf %lf %lf %c",
 			&setter->lookfrom.x, &setter->lookfrom.y, &setter->lookfrom.z,
 			&setter->lookat.x, &setter->lookat.y, &setter->lookat.z,
-			&setter->vfov, 
+			&setter->vfov,
 			&setter->vup.x, &setter->vup.y, &setter->vup.z,
 			&setter->aperture, &setter->focus_dist, &extra) != 13
-			|| extra != '\0')
+		|| extra != '\0')
 	{
 		setter->no_option = 1;
 		if (ft_sscanf(line, "%lf,%lf,%lf %lf,%lf,%lf %lf %c",
-			&setter->lookfrom.x, &setter->lookfrom.y, &setter->lookfrom.z,
-			&setter->lookat.x, &setter->lookat.y, &setter->lookat.z,
-			&setter->vfov, &extra) != 8
-			|| extra != '\0') 
+				&setter->lookfrom.x, &setter->lookfrom.y, &setter->lookfrom.z,
+				&setter->lookat.x, &setter->lookat.y, &setter->lookat.z,
+				&setter->vfov, &extra) != 8
+			|| extra != '\0')
 			exit_with_error("Error:camera\n");
 	}
 }
